05c81f85e6fd77f432bb5298457e77f5d263e268
Aakarsh <aakarshdwivedi43@gmail.com>
2024-06-05 23:24:47 +0530

    js

---
+++ main.js
@@ -0,0 +1,51 @@
+// var , let , const
+console.log("HELLO WORLD");
+ let fname="DEKu";
+ let ismarried=true;
+ //if else
+ if(ismarried==true)
+    {
+        console.log(fname+"  is married");
+    }
+else{
+    console.log(fname+" is not married");
+}
+
+// checking even no or not
+
+let a=19;
+if(a%2==0)
+    {
+        console.log(a + " is even")
+    }
+else
+{
+    console.log(a + " is odd")
+}
+
+
+// for loop
+// write a program that counts from 0-100 and prints(for loop)
+let ans=0;
+for(let i=0;i<100;i++){
+    ans=ans+1;
+    }
+    console.log(ans);
+
+
+// write a program to greet a person given their first and last name
+let firstname="Dekuu";
+let lastname="Midoriya";
+console.log("Hello! "+ firstname+ " "+ lastname);
+
+
+// write a program that greets a person based on their gender(if else)
+let age=18;
+if(age<18)
+    {
+        console("Yo broo!");
+    }
+else{
+    console("Namaste!");
+}
+
