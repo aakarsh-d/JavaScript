47b50599685df77b5ededc9f04054feca80cdec1
Aakarsh <aakarshdwivedi43@gmail.com>
2024-06-08 22:55:58 +0530

    js

--- main.js
+++ main.js
@@ -81,3 +81,19 @@ const user1={
 }
 console.log(user1["gender"])
 // const value=[1,2,3];
+
+
+
+
+
+
+//reverse an array
+let ar=[1,2,3,4,5];
+let p=[];
+for(let i=ar.length;i<0;i++){
+    p[i]=ar[i];
+}
+for(let i=0;i<p.length;i++){
+console.log(p[i])
+}
+    
